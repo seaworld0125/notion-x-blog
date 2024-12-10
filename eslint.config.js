342550c64c2df4d3456861c8cd5a0f99b4258e04
taekyoung <taekyoung@jobis.co>
2024-12-10 10:52:13 +0900

    feat: change theme

--- eslint.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  extends: ['next', 'next/core-web-vitals'],
-  rules: {
-    '@typescript-eslint/no-unused-vars': ['error', { varsIgnorePattern: '^_' }],
-  },
-};
---
+++ site.config.js
@@ -0,0 +1,75 @@
+const CONFIG = {
+  // profile setting (required)
+  profile: {
+    name: "taekyoung",
+    image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
+    role: "backend developer",
+    bio: "spring boot is my favorite",
+    email: "kyoung@kakao.com",
+    linkedin: "taekyoung-kim125",
+    github: "seaworld0125",
+    instagram: "",
+  },
+  projects: [
+    {
+      name: `taekyoung-blog`,
+      href: "https://github.com/seaworld0125/notion-x-blog",
+    },
+  ],
+  // blog setting (required)
+  blog: {
+    title: "taekyoung-log",
+    description: "welcome to taekyoung-log!",
+    scheme: "system", // 'light' | 'dark' | 'system'
+  },
+
+  // CONFIG configration (required)
+  link: "https://notion-x-blog.vercel.app",
+  since: null, // If leave this empty, current year will be used.
+  lang: "ko-KR", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
+  ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
+
+  // notion configuration (required)
+  notionConfig: {
+    pageId: process.env.NOTION_PAGE_ID,
+  },
+
+  // plugin configuration (optional)
+  googleAnalytics: {
+    enable: false,
+    config: {
+      measurementId: process.env.NEXT_PUBLIC_GOOGLE_MEASUREMENT_ID || "",
+    },
+  },
+  googleSearchConsole: {
+    enable: false,
+    config: {
+      siteVerification: process.env.NEXT_PUBLIC_GOOGLE_SITE_VERIFICATION || "",
+    },
+  },
+  naverSearchAdvisor: {
+    enable: false,
+    config: {
+      siteVerification: process.env.NEXT_PUBLIC_NAVER_SITE_VERIFICATION || "",
+    },
+  },
+  utterances: {
+    enable: true,
+    config: {
+      repo: process.env.NEXT_PUBLIC_UTTERANCES_REPO || "",
+      "issue-term": "og:title",
+      label: "💬 Utterances",
+    },
+  },
+  cusdis: {
+    enable: false,
+    config: {
+      host: "https://cusdis.com",
+      appid: "", // Embed Code -> data-app-id value
+    },
+  },
+  isProd: process.env.VERCEL_ENV === "production", // distinguish between development and production environment (ref: https://vercel.com/docs/environment-variables#system-environment-variables)
+  revalidateTime: 21600 * 7, // revalidate time for [slug], index
+}
+
+module.exports = { CONFIG }
